c024215bce5f51f9f996294c23fc2b709c3a50b8
b21mihpa <b21mihpa@student.his.se>
2022-04-07 10:51:02 +0200

    bound: MountainAdapter to RecyclerView

--- app/src/main/java/com/example/recyclerviewexperiment/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewexperiment/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.recyclerviewexperiment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -19,5 +20,8 @@ public class MainActivity extends AppCompatActivity {
         List<String> mountains = Arrays.asList("Mount Everest", "K2", "Kangchenjunga", "Lhotse", "Makalu", "Cho Oyu", "Manaslu");
 
         recyclerView = findViewById(R.id.my_recycler_view);
+        MountainAdapter mountainAdapter = new MountainAdapter(mountains);
+        recyclerView.setAdapter(mountainAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
     }
 }
