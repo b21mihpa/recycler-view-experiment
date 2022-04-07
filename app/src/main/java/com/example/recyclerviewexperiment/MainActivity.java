963cbfa0e7cee2634ee54b91d0175252610f98d4
b21mihpa <b21mihpa@student.his.se>
2022-04-07 09:48:22 +0200

    added: data for mountains

--- app/src/main/java/com/example/recyclerviewexperiment/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewexperiment/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.recyclerviewexperiment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> mountains = Arrays.asList("Mount Everest", "K2", "Kangchenjunga", "Lhotse", "Makalu", "Cho Oyu", "Manaslu");
+
+        recyclerView = findViewById(R.id.my_recycler_view);
     }
 }
